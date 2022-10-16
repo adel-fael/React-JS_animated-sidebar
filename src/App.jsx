89b381953d569599bfb91fcdcffcd303a4084e43
Adel-Fael <new19.adel@gmail.com>
2022-10-16 12:40:25 +0300

    adding absolute imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import SideBar from './components/SideBar'
+import SideBar from "@/components/SideBar";
 
 function App() {
-
-
   return (
     <div className="mx-auto mt-8">
-      <h1 className='text-4xl text-center'>Main Content</h1>
-      <SideBar/>
+      <h1 className="text-4xl text-center">Main Content</h1>
+      <SideBar />
     </div>
   )
 }
